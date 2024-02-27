3dc141f8be3a3432a9666333c663d864b1374ead
suresh akula <akulasuresh102@gmail.com>
2024-02-27 22:11:28 +0530

    new file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+let firstName = "Venkata Suresh";
+let lastName = "Akula";
+let age = 25;
+let adress = "vinjamuru";
+let mobileNumber = 6305648563;
+
+console.table({firstName, lastName, age, adress, mobileNumber});
+
