c2dbd05f28bc0eadb68ee2c82ef1f9917c2538dd
suresh akula <akulasuresh102@gmail.com>
2024-02-27 22:56:27 +0530

    new file

---
+++ 01_basics/03_typeConversion.js
@@ -0,0 +1,14 @@
+let number = "123d";
+let numberValue = Number(number);
+
+console.log(numberValue);
+
+console.log(typeof numberValue);
+let stringValue = String(numberValue);
+
+console.log(stringValue);
+console.log(typeof stringValue)
+let booleanValue = Boolean(numberValue);
+
+console.log(booleanValue);
+console.log(typeof booleanValue)
