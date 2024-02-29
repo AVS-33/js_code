7f83c473af3fcd99882a5d943fa2c52d5e6706ef
suresh akula <akulasuresh102@gmail.com>
2024-02-29 22:25:57 +0530

    new file

---
+++ 01_basics/comparisions.js
@@ -0,0 +1,11 @@
+// comparision operators in js
+
+let a = 10;
+let b = 20;
+
+console.log(a > b); // false
+console.log(a < b); // true
+console.log(a >= b); // false
+console.log(a <= b); // true
+console.log(a == b); // false
+console.log(a != b); // true
